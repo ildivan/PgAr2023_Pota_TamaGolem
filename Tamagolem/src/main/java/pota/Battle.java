8936107db8046f47808180182be26ec8f8e14b27
Ivan Abrami <Ivanpitbull2512@gmail.com>
2023-05-08 16:55:47 +0200

    Added returnStonesToStorage
    now when a golem is dead its element stones are returned to the storage  in common between the two players

--- Tamagolem/src/main/java/pota/Battle.java
+++ Tamagolem/src/main/java/pota/Battle.java
@@ -11,19 +11,24 @@ import java.util.List;
 
 public class Battle {
     private ElementsBalance balance;
+    private int[] elementStoneStorage;
     private Player firstPlayer;
     private Player secondPlayer;
 
     public Battle(int numberOfElements, int golemHealth) {
         balance = ElementsBalance.newRandomBalance(numberOfElements,golemHealth);
-
         int numberOfStonesPerGolem = (int) Math.ceil((numberOfElements+1.0)/3.0) + 1;
         int numberOfGolemsPerTeam
                 = (int) Math.ceil((numberOfElements-1.0)*(numberOfElements-2.0)/(2.0*numberOfStonesPerGolem));
+
+        int  numberOfStonesPerElement = (int) Math.ceil(2.0*numberOfGolemsPerTeam*numberOfStonesPerGolem/numberOfElements);
+        elementStoneStorage = new int[numberOfElements];
+        Arrays.fill(elementStoneStorage,numberOfStonesPerElement);
+
         firstPlayer = new Player(numberOfGolemsPerTeam,golemHealth);
         secondPlayer = new Player(numberOfGolemsPerTeam,golemHealth);
         firstPlayer.nextGolem();
-        firstPlayer.nextGolem();
+        secondPlayer.nextGolem();
     }
 
     public void start(){
@@ -35,8 +40,10 @@ public class Battle {
                     nextAttack();
                 } catch (AttackWithDeadGolemException e) {
                     if (!firstPlayer.getCurrentGolem().isAlive()) {
+                        returnStonesToStorage(firstPlayer.getCurrentGolem());
                         summonFirstPlayerGolem(Element.WATER);
                     } else {
+                        returnStonesToStorage(secondPlayer.getCurrentGolem());
                         summonSecondPlayerGolem(Element.CHAOS);
                     }
                 }
@@ -47,6 +54,13 @@ public class Battle {
             }else{
                 System.out.println("First player wins!");
             }
+        }
+    }
+
+    private void returnStonesToStorage(TamaGolem currentGolem) {
+        List<Element> stones = currentGolem.getElementStones();
+        for (Element stone : stones) {
+            elementStoneStorage[stone.ordinal()]++;
         }
     }
 
