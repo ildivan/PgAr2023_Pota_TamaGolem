280f913c7b9efa09468dfe8dc371150c5eb3eaa8
Ivan Abrami <Ivanpitbull2512@gmail.com>
2023-05-10 23:21:53 +0200

    Commented Main and createg getNumberOfElements

--- Tamagolem/src/main/java/pota/Main.java
+++ Tamagolem/src/main/java/pota/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Player player1 = new Player("Giocatore 1");
         Player player2 = new Player("Giocatore 2");
-
+        //main menu
         do {
             switch(TamaMenu.mainMenu()) {
                 case 1 -> newGame(player1, player2);
@@ -18,24 +18,30 @@ public class Main {
         } while(true);
     }
 
+    //Starts a new game, the number of elements will be prompted.
     public static void newGame(Player player1, Player player2) {
-        int numberOfElements=0;
         Menu.clearConsole();
         System.out.println(PrettyStrings.frame("Battaglia", 60, true, false));
         
-        numberOfElements = InputData.readInteger("Quanti elementi vuoi evocare [min 3, max 10]: ");
-        while(numberOfElements>10 | numberOfElements<3){
-            System.out.println("Puoi evocare un minimo di 3 ed un massimo di 10 elementi!");
-            TamaMenu.pauseProgram();
-            Menu.clearConsole(); 
-            numberOfElements = InputData.readInteger("Quanti elementi vuoi evocare [min 3, max 10]: ");
-        }
-       
-        Battle battle = new Battle(player1, player2, numberOfElements, 100); //NumberOfElements must be between 3 and 10
+        int numberOfElements = getNumberOfElements();//numberOfElements must be between 3 and 10
+        Battle battle = new Battle(player1, player2, numberOfElements, 100);
         battle.start();
         TamaMenu.pressEnterToContinue();
     }
 
+    //Gets a number of elements within bounds from the  user.
+    private static int getNumberOfElements() {
+        int numberOfElements = InputData.readInteger("Quanti elementi vuoi evocare [min 3, max 10]: ");
+        while(numberOfElements>10 | numberOfElements<3){
+            System.out.println("Puoi evocare un minimo di 3 ed un massimo di 10 elementi!");
+            TamaMenu.pauseProgram();
+            Menu.clearConsole();
+            numberOfElements = InputData.readInteger("Quanti elementi vuoi evocare [min 3, max 10]: ");
+        }
+        return numberOfElements;
+    }
+
+    //Menu used to change the names of the players.
     public static void setPlayersNames(Player player1, Player player2) {
         do {
             switch(TamaMenu.setPlayersNamesMenu(player1, player2)) {
