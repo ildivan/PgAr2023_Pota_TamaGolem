0fc8124d1ba0694762628541214b88098ba58e86
Ivan Abrami <Ivanpitbull2512@gmail.com>
2023-05-10 23:17:32 +0200

    Commented Team

--- Tamagolem/src/main/java/pota/Team.java
+++ Tamagolem/src/main/java/pota/Team.java
@@ -2,23 +2,36 @@ package pota;
 
 import pota.error.NoMoreGolemsException;
 
-/* count of Golems remaining to each player, with the appropriate remaining health points.
- * They are reduced during the game according to the damage taken.
+/**
+ * Represents the TamaGolem instances a Player has during a Battle,
+ * They are given to the Player sequentially as they are not all created in the beginning.
  */
 public class Team {
-    private TamaGolem currentGolem;
-    private int remainingGolems;
-    private final int golemHealthPoints;
-    
+    private TamaGolem currentGolem; //The current TamaGolem instance used in battle.
+    private int remainingGolems; //How many golems are remaining after currentGolem dies.
+    private final int golemHealthPoints; //The amount of health the golems are created with.
+
+    /**
+     * @param numberOfGolems the numbe of golems the player will be able to generate.
+     * @param golemHealthPoints the health every golem will be assigned at creation.
+     */
     public Team(int numberOfGolems, int golemHealthPoints) {
         this.remainingGolems = numberOfGolems;
         this.golemHealthPoints = golemHealthPoints;
     }
 
+    /**
+     * @return The current TamaGolem instance.
+     */
     public TamaGolem getCurrentGolem() {
         return currentGolem;
     }
 
+    /**
+     * If possible, it is created a new golem to use in battle,
+     * if not an exception is thrown.
+     * @see NoMoreGolemsException
+     */
     public void nextGolem() {
         if (remainingGolems > 0) {
             remainingGolems--;
